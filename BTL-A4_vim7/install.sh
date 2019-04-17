77bf9a1965b0e0ea71de03c3aba681d6304b30bc
Ramón Astudillo <ramon@astudillo.com>
2019-04-16 21:20:21 -0400

    Add tmux config, fix folder name

--- BTL-A4_vim7/install.sh
+++ BTL-A4_vim7/install.sh
@@ -3,15 +3,18 @@ set -o pipefail
 set -o errexit 
 
 # Sanity check
-[ ! -d BTL-A4/ ] && echo "bash BTL-A4/install.sh" && exit
+[ ! -d BTL-A4_vim7/ ] && echo "bash BTL-A4_vim7/install.sh" && exit
 
 # vim7
 # Pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # configs
-cp BTL-A4/.vimrc $HOME
-cp -R BTL-A4/.vim $HOME
+cp BTL-A4_vim7/.vimrc $HOME
+cp -R BTL-A4_vim7/.vim $HOME
 # python-mode 
 cd $HOME/.vim/bundle
 git clone --recursive https://github.com/python-mode/python-mode.git
+
+# tmux
+cp BTL-A4_vim7/.tmux.conf $HOME
--- BTL-A4_vim8/install.sh
+++ BTL-A4_vim8/install.sh
@@ -3,14 +3,17 @@ set -o pipefail
 set -o errexit 
 
 # Sanity check
-[ ! -d BTL-A4/ ] && echo "bash BTL-A4/install.sh" && exit
+[ ! -d BTL-A4_vim8/ ] && echo "bash BTL-A4_vim8/install.sh" && exit
 
 # vim
-cp BTL-A4/.vimrc $HOME
-cp -R BTL-A4/.vim $HOME
+cp BTL-A4_vim8/.vimrc $HOME
+cp -R BTL-A4_vim8/.vim $HOME
 
 # python-mode for vim8
 [ ! -d $HOME/.vim/pack/python-mode/start ] && mkdir -p $HOME/.vim/pack/python-mode/start
 cd ~/.vim/pack/python-mode/start
 git clone --recursive https://github.com/python-mode/python-mode.git
 cd python-mode
+
+# tmux
+cp BTL-A4_vim8/.tmux.conf $HOME
